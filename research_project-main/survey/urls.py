454d10e3cfe4304dc091344d5f319d4a8bb53d72
larissamariadesousa <larissamariadesousa@gmail.com>
2026-03-27 10:48:18 +0000

    fix: remove duplicate subfolder

--- research_project-main/survey/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',              views.survey_form,  name='survey_form'),
-    path('thanks/',       views.thanks,       name='thanks'),
-    path('results/',      views.results,      name='results'),
-    path('results/data/', views.results_data, name='results_data'),
-]
